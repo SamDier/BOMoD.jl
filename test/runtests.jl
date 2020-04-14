656b0ac41fddc9b881e8de733201e733823a03de
Dierickx Sam <sam.dierickx5@gmail.com>
2020-04-14 11:18:16 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,10 @@
 using Test
 using BOMoD
+using BOMoD: filter_constrain
 using Random
 using StatsBase
 
-@testset "BoMod" begin
+@testset "BOMod" begin
     include("Mod_test.jl")
     include("Filter_test.jl")
     include("Space_test.jl")
