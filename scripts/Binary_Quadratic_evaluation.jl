b4a4696acdb7722cca3200a239986676e1962186
Dierickx Sam <sam.dierickx5@gmail.com>
2020-04-15 09:03:22 +0200

    update

--- scripts/Binary_Quadratic_evaluation.jl
+++ scripts/Binary_Quadratic_evaluation.jl
@@ -17,11 +17,12 @@ mod1 = Mod(Symbol(1))
 
 # no constrains model of lenght 10, not all constructs are generated
 all_moduels = Group_Mod([mod0,mod1])
+lenght_c = 10
 σ²_mean = 10^-6
 # the design
-design = construct_design(all_moduels,lenght_c)
+design = construct_design(all_moduels,lenght_c,order = true)
 # the space
-space = BOMoD.getspace(design)
+space = getspace(design)
 
 
 
@@ -33,12 +34,11 @@ space = BOMoD.getspace(design)
 n_Q = 1
 # number of simimulations
 const n_iter = 10
-const n_cycles = 4
+const n_cycles = 20
 # number of BO cycles
 ####################
 #toy_data settings
 ###################
-const lenght_c = 10
 const λ = 0
 const Lc = 10
 
@@ -79,23 +79,23 @@ for i in 1:n_iter
         # sample random
         # filter sample constructs
         x_test_random = [construct for construct in space if sum([isequal(construct,train_con) for train_con in construct_train_random]) == 0 ]
-        myrandompoints = StatsBase.sample(rng,x_test_random,n_samples)
+        myrandompoints = sample(rng,x_test_random,n_samples)
 
 
         # construct generated y-values
-        y_train = map(x -> unpackconstruct(x) |> (x -> quad(x,Q,λ)),construct_train_BO)
+        y_train = map(x -> vec(unpackconstruct(x)) |> (x -> quad(x,Q,λ)),construct_train_BO)
         # use BO
         x_test_BO = [construct for construct in space if sum([isequal(construct,train_con) for train_con in construct_train_BO]) == 0 ]
         Model = gp_optimised(construct_train_BO,y_train,Test_kernel)
-        new_predictions = save_thompson_sampling(Model.GP_model,x_test_BO,10,10^-6)
+        new_predictions = thompson_sampling_fast(Model.GP_model,x_test_BO,10,10^-6)
 
         #save new constructs
         construct_train_random = [construct_train_BO;myrandompoints[:,1]]
-        construct_train_BO = [construct_train_BO;new_predictions[:,1]]
+        construct_train_BO = [construct_train_BO;new_predictions[1]]
 
         #max values
-        max_random[i,j] =  maximum(map(x -> unpackconstruct(x) |> (x -> quad(x,Q,λ)),construct_train_random))
-        max_model[i,j] = maximum(map(x -> unpackconstruct(x) |> (x -> quad(x,Q,λ)),construct_train_BO ))
+        max_random[i,j] =  maximum(map(x -> vec(unpackconstruct(x) )|> (x -> quad(x,Q,λ)),construct_train_random))
+        max_model[i,j] = maximum(map(x -> vec(unpackconstruct(x))|> (x -> quad(x,Q,λ)),construct_train_BO ))
 
     end
 end
--- scripts/example.jl
+++ scripts/example.jl
@@ -2,6 +2,9 @@ using BOMoD
 using Random
 using StatsBase
 using LinearAlgebra
+using Stheno
+using Plots
+using StatsPlots
 #using Optim
 #import Optim: minimizer
 include("Binary_Quadratic.jl")
@@ -9,20 +12,19 @@ include("Binary_Quadratic.jl")
 rng = MersenneTwister(1)
 # set up BOMoD use
 # make binary moduels
-mod0 = Mod(Symbol(0))
-mod1 = Mod(Symbol(1))
+Mod0 = Mod(Symbol(0))
+Mod1 = Mod(Symbol(1))
 lenght_c = 5
 # no constrains model of lenght 10, not all constructs are generated
-all_moduels =Group_Mod([mod0,mod1])
+all_moduels =Group_Mod([Mod0,Mod1])
 σ²_mean = 10^-6
 # the design
 design = construct_design(all_moduels,lenght_c,order = true)
 # the space
-a_space = BOMoD.getspace(design)
+a_space = getspace(design)
 
 # settings of the toy datatsettings
 
-lenght_c = 10
 λ = 0
 Lc = 10
 # setting up the toydata
@@ -32,13 +34,16 @@ const Q = compute_decay(rng,lenght_c,Lc)
 # first sampling step
 
 points = sample(rng,a_space,10,with_index = true )
-unpackconstruct(points[:,1][1])
+
 #get y_train values
 # zet up the generated data
-y_train = map(x -> unpackconstruct(x) |> (x -> quad(x,Q,λ)),points[:,1])
+y_train = map(x -> vec(unpackconstruct(x)) |> (x -> quad(x,Q,λ)),points[:,1])
 constructs_train = points[:,1]
 MyKernel = LevStehnoexp(1)
 Model = gp_optimised(constructs_train,y_train,MyKernel)
+
+
+
 
 # Needs improvement to prevent the explicit generation of all constructs.
 all_constructs = map(x -> unpackconstruct(x),collect(space))
@@ -46,12 +51,28 @@ x_test = [con for (index,con) in enumerate(all_constructs) if index ∉ points[:
 x_test_space = [con for (index,con) in enumerate(space) if index ∉ points[:,2]]
 constructs_test = [construct for construct in space if sum([isequal(construct,train_con) for train_con in points[:,1]]) == 0 ]
 
+new_predictions = thompson_sampling_fast(Model.GP_model,constructs_test,10,10^-6)
+new_predictions[1]
+#evaluate TS
+##
+m = marginals(Model.GP_model(y_train),10^-6)
 
 
+m = marginals(Model.GP_model(constructs_test,10^-6))
+save = Matrix{Float64}(undef,22,1000)
 
-in points[:,1])), space.space)
-new_predictions = save_thompson_sampling(Model.GP_model,x_test_space,10,10^-6)
+permutedims(hcat(rand.(m,5)...) )
+for i in 1:1000
+    save[:,i]= map(x-> rand(x),m)
+end
+save2 = rand(Model.GP_model(constructs_test,10^-6),1000)
 
-a_space[138] in points[:,1]
+gr()
 
-myfilter(x::Ordered_Construct) = sum([isequal(i,x) for i in points[:,1]]) == 0 ? x : nothing
+plt  = plot(title = " y* - max(y)",legend = false);
+for i in 1:10
+    boxplot!(plt,[string("Stheno","$i")],save2[i,:],label = string("Stheno"))
+    boxplot!(plt,[string("myrand","$i")],save[i,:],label = string("myrand"))
+end
+
+plot(plt)
