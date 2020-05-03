a14e9d3612b76c302528b173c508128bf10bcd57
Dierickx Sam <sam.dierickx5@gmail.com>
2020-05-03 18:35:42 +0200

    :tada: first neigberhood

--- src/BO/nb.jl
+++ src/BO/nb.jl
@@ -27,3 +27,19 @@ function neibourhood_1(construct::Ordered_Construct,mod::Group_Mod)
     end
     return d_n
 end
+
+"""
+d_neibourhood(construct::Ordered_Construct,mod::Group_Mod)
+generates all constructs
+"""
+function neibourhood_1(construct::UnorderedConstruct,mod::Group_Mod)
+    d_n = Vector{Ordered_Construct}(undef,length(mod))
+    for i in 1:length(construct)
+        if i > 1
+            d_n[i] = construct[1:i-1].*mod.m.*construct[i+1:end]
+        else
+            d_n[i] = mod.m*construct[i+1:end]
+        end
+    end
+    return d_n
+end
