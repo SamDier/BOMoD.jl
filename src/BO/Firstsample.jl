89afc1893a3f42f0cc5e6c1bc881f19f4b1425aa
Dierickx Sam <sam.dierickx5@gmail.com>
2020-05-08 00:05:13 +0200

    :tade: sample evaluation

--- src/BO/Firstsample.jl
+++ src/BO/Firstsample.jl
@@ -1,12 +1,29 @@
+"""
+    pairwised(X,d)
+Returns the pairwise distances `d` between every vector stored in X.
+"""
+
 function pairwised(X,d)
     m = length(X)
-    return [evaluted(d,xi,xj) for xi in (1:m-1) for j in (i+1):m]
+    return [evaluate(d,X[i],X[j]) for i in (1:m-1) for j in (i+1):m]
 end
 
+"""
+    Compear_distance(Bᵢ,Bⱼ,d;rev=false)
 
-function Compear_distance(Bᵢ,Bⱼ,d)
-    Dᵢ = pairwised(Bᵢ,d) |> sort
-    Dⱼ = pairwised(Bⱼ,d) |> sort
+ Returns the most spacefilling subset given Bᵢ of Bⱼ.
+ The distance `d` is used to evaluate the pairwise distance in every set.
+ The `rev` arguments allow to adapt the function to different distances.
+ Set rev = false if the distance decreases as two vectors are more similar.
+ e.g. Levenshtein distance.
+ Set rev = true if the distance increase as two vectors are more similar
+ e.g. Cosine  simularity
+"""
+function Compear_distance(Bᵢ,Bⱼ,d;rev=false)
+    Dᵢ = pairwised(Bᵢ,d) |> (t -> sort(t,rev=rev))
+    Dⱼ = pairwised(Bⱼ,d) |> (t -> sort(t,rev=rev))
+    println(Dᵢ)for
+    println(Dⱼ)
     for (di,dj) in zip(Dᵢ,Dⱼ)
         if di < dj
             return Bⱼ
@@ -14,5 +31,6 @@ function Compear_distance(Bᵢ,Bⱼ,d)
             return Bᵢ
         end
     end
+    @info "equaly spaced and first set is returned"
     return Bᵢ
 end
