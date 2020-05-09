3567940b67413bea4fd3a98cdc94988ca0dc4bae
Dierickx Sam <sam.dierickx5@gmail.com>
2020-05-09 16:13:54 +0200

    :zap:  kernels

--- src/BO/Kernels.jl
+++ src/BO/Kernels.jl
@@ -39,11 +39,11 @@ struct QgramKernel{T <: QGramDistance } <: Kernel
     d::T
 end
 
-ew(k::QgramKernel, x::AbstractVector) = [1-evaluate(k.d,xᵢ,xᵢ)) for xᵢ in x]
-ew(k::QgramKernel, x::AbstractVector, x′::AbstractVector) = [1-evaluate(k.d,xᵢ,xⱼ)) for (xᵢ,xⱼ) in zip(x, x′)]
+ew(k::QgramKernel, x::AbstractVector) = [1-evaluate(k.d,xᵢ,xᵢ) for xᵢ in x]
+ew(k::QgramKernel, x::AbstractVector, x′::AbstractVector) = [1-evaluate(k.d,xᵢ,xⱼ) for (xᵢ,xⱼ) in zip(x, x′)]
 
-pw(k::QgramKernel, x::AbstractVector) = reshape([1-evaluate(k.d,xᵢ,xⱼ)) for xᵢ in x for xⱼ in x] ,(length(x),length(x)))
-pw(k::QgramKernel, x::AbstractVector, x′::AbstractVector) = reshape([1-evaluate(k.d,xᵢ,xⱼ)) for xⱼ in x′ for xᵢ in x] ,(length(x),length(x′)))
+pw(k::QgramKernel, x::AbstractVector) = reshape([1-evaluate(k.d,xᵢ,xⱼ) for xᵢ in x for xⱼ in x] ,(length(x),length(x)))
+pw(k::QgramKernel, x::AbstractVector, x′::AbstractVector) = reshape([1-evaluate(k.d,xᵢ,xⱼ) for xⱼ in x′ for xᵢ in x] ,(length(x),length(x′)))
 
 ###
 #EditDistancesKernel
@@ -95,21 +95,26 @@ More information can be found on [Stheno]@ref(https://github.com/willtebbutt/Sth
 
 pw(k::EditDistancesKernel, x::AbstractVector, x′::AbstractVector) = reshape([exp(-evaluate(k.d,xᵢ,xⱼ)) for xⱼ in x′ for xᵢ in x] ,(length(x),length(x′)))
 
+######
+# Compese two Kernels
+######
+
+
 ####
-#Diffusion
+#Kernels on a Graph
 ####
+
 """
-    Diffusion
+    KernelGraph{T}
 allow to use a Diffion kernel on a graph.
 The graph is constructed based on all combinations.
 """
-abstract type KernelGraph{T} <:Kernel{T}
-struct DiffusionKernel{T}  <: KernelGraph{T}
-    β::T
- end
-struct Prandomwalk{T}   <: KernelGraph
+abstract type KernelGraph{T} <: Kernel end
+
+
+struct DiffusionKernel{T}  <: KernelGraph{T} end
+struct Prandomwalk{T}   <: KernelGraph{T}
     p::Int
-    a::{T}
  end
 
 function setupgraph(S,k::KernelGraph,edgerule::EdgeRule)
@@ -130,48 +135,14 @@ Lecture Notes in Computer Science, vol 2777. Springer, Berlin, Heidelberg
 kernelgraph(L,gk::DiffusionKernel,β) = exp(-β*L)
 
 """
-     kernelgraph(l::AbstractArray,gk::Diffusion;β = 1)
+     kernelgraph(l::AbstractArray,gk::Diffusion;a = 1)
 p-random walk kernel on the normalized laplacian `l` wiht hyper parameter a
-
+with ``a geq 2``
 Smola A.J., Kondor R. (2003) Kernels and Regularization on Graphs.
 In: Schölkopf B., Warmuth M.K. (eds) Learning Theory and Kernel Machines.
 Lecture Notes in Computer Science, vol 2777. Springer, Berlin, Heidelberg
 """
-function kernelgraph(L,gk::Prandomwalk,a,p)
+function kernelgraph(L,gk::Prandomwalk,a)
     @assert a >= 2 "a has to be larger than 2"
-    return (a*I - L)^(p)
+    return (a*I - L)^(gk.p)
 end
-
-
-
-
-
-
-
-@doc raw"""
-    cossim(xᵢ,xⱼ)
-Returns the cosine  similarity  between two strings.
-The first the strings are transformed in a vector-embedding
-and then between the obtained vectors the cosine similarity.
-
-``cossim(x_i,x_j))= dot(x_i, x_j) / (norm(x_i) * norm(x_j))``
-
-"""
-
-function cossim(xᵢ,xⱼ)
-    letter2index = Dict( letter => index for (index,letter) in enumerate(Set([xᵢ;xⱼ])))
-    xi_v = _word2vec(xᵢ,letter2index)
-    xj_v = _word2vec(xⱼ,letter2index)
-    return dot(xi_v,xj_v)/(norm(xi_v) * norm(xj_v))
-end
-
-
-struct CosStheno <: Kernel end
-
-
-ew(::CosStheno , x::AbstractVector{N} where N) = ones(length(x))
-ew(::CosStheno , x::AbstractVector{N} where N, x′::AbstractVector{N} where N) = [cossim(xᵢ,xⱼ) for (xᵢ,xⱼ) in zip(x, x′)]
-
-
-pw(k:: CosStheno , x::AbstractVector{N} where N) = reshape([cossim(xᵢ,xⱼ) for xᵢ in x for xⱼ in x] ,(length(x),length(x)))
-pw(k:: CosStheno , x::AbstractVector{N} where N, x′::AbstractVector{N} where N) = reshape([cossim(xᵢ,xⱼ) for xⱼ in x′ for xᵢ in x] ,(length(x),length(x′)))
