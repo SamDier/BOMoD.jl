2e8e947acee45b05d0daed69a67f96347681f25d
Dierickx Sam <sam.dierickx5@gmail.com>
2020-05-16 21:25:25 +0200

    Update GP_model.jl

--- src/BO/GP_model.jl
+++ src/BO/GP_model.jl
@@ -16,9 +16,8 @@ Base.getindex(p::Hyperparmeter,i) = p.parm[i]
 """
         GPModel{T <: Any}
 
-Struct to store fitted model with given Kernel K and hyperparmeters θ
+Struct to store fitted model f̂ with given Kernel K and hyperparmeters θ
 """
-
 struct GPModel{Tf <: Any, Ts <: Any}
         f̂::Tf
         K::Kernel
@@ -53,8 +52,6 @@ end
    on maximum likelihood estimation, default = false.
 
 """
-
-
 function fit_gp(x_train,y_train,k::Kernel,mod::GroupMod; θ = [1], σ²_n = 10^-6,optimise = false)
         v_train = transform_data(x_train,mod::GroupMod,k)
         #set Gaussian procces in Stheno framework
@@ -92,14 +89,12 @@ end
 
   returns a GPModel with the posterior of the gp, the kernel, used hyperparameters
 """
-
-
-function fit_gp_graph(S,x_train,y_train,k::KernelGraph,edgerule::EdgeRule;θ = [1,2],σ²_n = 10^-6,optimise = false)
+function fit_gp_graph(S,x_train,y_train,k::KernelGraph,edgerule::EdgeRule;θ = [1,1],σ²_n = 10^-6,optimise = false)
         @assert eltype(x_train) == Int "The Kernel graphs requiers the indexes as input"
         n_laplace = setupgraph(S,k,edgerule)
         #set Gaussian procces in Stheno framework
         if optimise
-                θ = gp_optimised(n_laplace,index_train,y_train,k,σ²_n)
+                θ = gp_optimised(n_laplace,x_train,y_train,k,σ²_n)
         end
         fGP,θ_save = _creatGP(n_laplace,k,θ)
         push!(θ_save, "σ²_n" => σ²_n)
@@ -149,7 +144,6 @@ end
         _creatGP(k::Kernel, α)
 Retuns an Stheno GP  model wiht a given kernel which is scaled with the parameter `α`.
 """
-
 function _creatGP(k::Kernel, θ)
         @assert length(θ) == 1 " Kernel has only one hyperparamter, α"
         fGP = θ[1]* GP(k, GPC())
@@ -162,7 +156,6 @@ end
         _creatGP(k::Kernel, θ)
 Retuns an Stheno GP  model with a diffusion kernel and hyperparameters θ.
 """
-
 function _creatGP(n_laplace,gk::DiffusionKernel,θ)
         α = θ[1]
         β = θ[2]
@@ -199,7 +192,6 @@ end
  The model is trained with the given datapoints x_train and corresponding values y_train
  Adds a small offset ,`σ²_n`, is the variance added to the model.
 """
-
 function nlml_stheno(θ_temp, x_train, y_train, k::Kernel, σ²_n)
     θ_exp = exp.(θ_temp) .+ 0.001
     f,_ = _creatGP(k,θ_exp)
@@ -213,7 +205,6 @@ end
  The model is trained with the given datapoints x_train and corresponding values y_train
  Adds a small offset ,`σ²_n`, is the variance added to the model.
 """
-
 function nlml_stheno(θ_temp,n_laplace,x_train, y_train, k::KernelGraph, σ²_n)
     θ_exp = exp.(θ_temp) .+ 1e-6
     f,_ = _creatGP(n_laplace,k,θ_exp)
@@ -236,7 +227,6 @@ end
  initial boundary of the algorithm
 
 """
-
 function gp_optimised(x_train,y_train,k::Kernel,σ²_n;min = -5, max = 10.0)
 #do perform optimisation
 results = Optim.optimize(θ_temp->nlml_stheno(θ_temp,x_train,y_train,k,σ²_n),min,max,
@@ -276,7 +266,6 @@ end
  Function transforms the vector of constructs into the vector embedding
  and then transforms the data into `ColVecs` type to fit linear model
 """
-
 function transform_data(x_in,mod::GroupMod,::Linear)
         # get vector embedding
         v_out = map(x -> _word2vec(x,mod),x_in)
@@ -286,9 +275,10 @@ end
 
 
 """
-        transform_data(x_train)
-"""
+        transform_data(x_in,mod::GroupMod,::Kernel)
+dummy tranform data, for non-stheno kernels
 
+"""
 transform_data(x_in,mod::GroupMod,::Kernel) = x_in
 
 
