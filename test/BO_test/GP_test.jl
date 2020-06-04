090a7bbec0e86640d5591af99d576242228e2227
Dierickx Sam <sam.dierickx5@gmail.com>
2020-06-04 23:33:49 +0200

    :bug:

--- test/BO_test/GP_test.jl
+++ test/BO_test/GP_test.jl
@@ -1,16 +1,16 @@
+@testset "GP2" begin
 
-    s2 = ["b","b","c","d"];
-    s3 = ["b","b","c","c"];
-    s4 = ["a","c","c","d"];
+    s1 = ["a","b","c","d"]
+    s2 = ["b","b","c","d"]
+    s3 = ["b","b","c","c"]
+    s4 = ["a","c","c","d"]
     m = groupmod(["a","b","c","d"])
     x1_test = [s1,s2,s3,s4]
     x1 = [OrderedConstruct([Mod(m) for m in s]) for s in [s1,s2,s3,s4]]
     y_train = randn(4)
-    typeof(_creatGP(k2,[1])[1])
 
-    ## _creatGP
     k2 = EditDistancesKernel(Levenshtein())
-    @testset "GP2" begin
+    @testset "GP1" begin
         isa(_creatGP(k2,[1])[1],Stheno.CompositeGP) == true
         _creatGP(k2,[1])[1].args[3].k == k2
     end
@@ -21,10 +21,8 @@
         _creatGP(k1,[1])[1].args[3].k == k1
     end
 
-    ## fit_gp
-
-
     @testset "fit_gp" begin
         isa(fit_gp(x1,y_train,k1,m),GPModel) == true
         isa(fit_gp(x1_test ,y_train,k2,m).f̂, Stheno.CompositeGP) == true
     end
+end
