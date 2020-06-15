536531cfdd645cf1d90efcf9d53382011f33f609
Dierickx Sam <sam.dierickx5@gmail.com>
2020-06-15 20:29:22 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -2,7 +2,7 @@ using Documenter
 using BOMoD
 
 makedocs(sitename="BOMoD.jl",
-        authors = "Sam Dierickx"
+        authors = "Sam Dierickx",
         format = :html,
        modules = [BOMoD])
 
